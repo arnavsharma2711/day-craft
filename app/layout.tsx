6e699c7ac0588251e62e4eff9217ffaeabf9133c
Arnav Sharma <arnavsharma2711@gmail.com>
2024-01-08 08:21:51 +0530

    Add new files and configurations for Next.js project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DayCraft',
+  description: 'Welcome to DayCraft – where we aim to help you craft your best life by managing your daily schedule, tracking progress, and setting and achieving meaningful goals.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="max-w-5xl w-full flex flex-col items-center justify-center font-mono text-lg lg:flex">
+        <h1 className="mb-4">
+          Project Name: DayCraft
+        </h1>
+        <h2>
+        Made with ❤️ by @arnavsharma2711
+        </h2>
+      </div>
+    </main>
+  )
+}
